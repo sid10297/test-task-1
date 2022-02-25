bcf5cee14487178cb071ce0b2a813b8bdee31a4c
Siddharth <siddharth10297@gmail.com>
2022-02-25 22:45:07 +0530

    Updated SelectDate component to keep selected date

--- src/components/SelectDate/index.jsx
+++ src/components/SelectDate/index.jsx
@@ -1,11 +1,38 @@
-import React from "react";
-import "./index.css";
+import React, { useState } from "react";
 import ReactDatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import dayjs from "dayjs";
 
-const SelectDate = ({label}) => {
+import "./index.css";
+
+const SelectDate = ({
+  label,
+  name,
+  onSelect,
+  date,
+  minDate = null,
+  maxDate = new Date(),
+}) => {
+  const [selectedDate, setSelectedDate] = useState(date ?? null);
+
+  const onChange = (date) => {
+    setSelectedDate(date);
+    const formattedDate = dayjs(date).format("YYYY-MM-DD");
+    onSelect && onSelect({ name, value: formattedDate });
+  };
+
   return (
-    <ReactDatePicker className='date_picker' placeholderText={label} />
+    <ReactDatePicker
+      className='date_picker'
+      dateFormat={'yyyy-MM-dd'}
+      name={name}
+      placeholderText={label}
+      minDate={minDate}
+      maxDate={maxDate}
+      selected={selectedDate}
+      onChange={onChange}
+      autoComplete={'off'}
+    />
   );
 };
 

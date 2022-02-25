52725bd0d260b9542e35edc58534b0e9658c5eb8
Siddharth <siddharth10297@gmail.com>
2022-02-25 22:49:38 +0530

    Integrated APIs
    Fetch projects
    
    Fetch gateways
    
    Create report

--- src/components/SectionHeaderForm/index.jsx
+++ src/components/SectionHeaderForm/index.jsx
@@ -1,35 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../Button";
 import SelectDate from "../SelectDate";
 import Select from "../Select";
 import "./index.css";
+import { getProjects } from "../../apis/project";
+import { getGateways } from "../../apis/gateway";
+import { createReport } from "../../apis/report";
 
 const SectionHeaderForm = () => {
+  const [projects, setProjects] = useState([]);
+  const [gateways, setGateways] = useState([]);
+  const [report, setReport] = useState(null);
+  const [reportParams, setReportParams] = useState({
+    to: "",
+    from: "",
+    projectId: "",
+    gatewayId: "",
+  });
+
+  useEffect(() => {
+    fetchProjects();
+    fetchGateways();
+  }, []);
+
+  const fetchProjects = async () => {
+    const {
+      data: { data },
+    } = await getProjects();
+    const projectOptions = data.map(({ projectId, name }) => ({
+      id: projectId,
+      name,
+    }));
+    setProjects(projectOptions);
+  };
+
+  const fetchGateways = async () => {
+    const {
+      data: { data },
+    } = await getGateways();
+    const gatewayOptions = data.map(({ gatewayId, name }) => ({
+      id: gatewayId,
+      name,
+    }));
+    setGateways(gatewayOptions);
+  };
+
+  const onSelect = ({ name, value }) =>
+    setReportParams({ ...reportParams, [name]: value });
+
+  const generateReport = async (e) => {
+    e.preventDefault();
+    const {data: {data}} = await createReport(reportParams);
+    console.log('Generated report: ', data);
+  };
+
   return (
-    <form className='section_form'>
+    <form className='section_form' onSubmit={generateReport}>
       <div className='select_projects m'>
-        <Select
-          select_id='select_projects'
-          select_name='select_projects'
-          option_label='All projects'
-          option_value='all_projects'
-        />
+        <Select name='projectId' options={projects} onSelect={onSelect} />
       </div>
       <div className='select_gateways m'>
-        <Select
-          select_id='select_gateways'
-          select_name='select_gateways'
-          option_label='All gateways'
-          option_value='gateways'
-        />
+        <Select name='gatewayId' options={gateways} onSelect={onSelect} />
       </div>
       <div className='select_start_date m'>
-        <SelectDate label="From date" />
+        <SelectDate label='From date' name={'from'} onSelect={onSelect} />
       </div>
       <div className='select_end_date m'>
-        <SelectDate label="To date" />
+        <SelectDate label='To date' name='to' onSelect={onSelect} />
       </div>
-      <Button label='Generate report' variant='gen_report_btn' />
+      <Button type='submit' label='Generate report' variant='gen_report_btn' />
     </form>
   );
 };

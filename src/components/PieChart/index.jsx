7e44f31200f7c706ac919466a178e67dcfa5ffdd
Siddharth <siddharth10297@gmail.com>
2022-02-28 16:15:40 +0530

    removed static color from chart header data

--- src/components/PieChart/index.jsx
+++ src/components/PieChart/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { PieChart, Pie, Cell } from "recharts";
 
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+const COLORS = ["#A259FF", "#6497B1", "#FFC107", "#F24E1E"];
 
 const Charts = ({ chartData }) => {
   return (
